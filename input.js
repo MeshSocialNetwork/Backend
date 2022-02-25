3f82606300d785974cc81c7751b9ae71158bbf1d
Lucas Pape <admin@lucaspape.de>
2022-02-25 11:01:23 +0100

    Fix input validation

--- input.js
+++ input.js
@@ -2,24 +2,36 @@ module.exports = class Input{
     verifyUsername(username){
         let regex = new RegExp('^(?=.{4,16}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$')
 
-        let match = username.match(regex)[0]
+        let match = username.match(regex)
 
-        return (match.length === username.length)
+        if(match){
+            return (match[0].length === username.length)
+        }else{
+            return false
+        }
     }
 
     verifyEmail(email){
         let regex = new RegExp('/^(([^<>()\\[\\]\\\\.,;:\\s@"]+(\\.[^<>()\\[\\]\\\\.,;:\\s@"]+)*)|(".+"))@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$/')
 
-        let match = email.match(regex)[0]
+        let match = email.match(regex)
 
-        return (match.length === email.length)
+        if(match){
+            return (match[0].length === email.length)
+        }else{
+            return false
+        }
     }
 
     verifyPassword(password){
         let regex = new RegExp('^.{8,64}$')
 
-        let match = password.match(regex)[0]
+        let match = password.match(regex)
 
-        return (match.length === password.length)
+        if(match){
+            return (match[0].length === password.length)
+        }else{
+            return false
+        }
     }
 }
